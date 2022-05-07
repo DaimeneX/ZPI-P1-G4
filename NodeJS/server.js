b0ca9811d031b2bbfb56b70f59b9f2eb9f6668f8
DaimeneX <DaimeneX@Onet.pl>
2022-05-07 23:03:08 +0200

    Aktualizacja Rest API
    Przerobienie całego serwera na POST
    Autoryzacja metodą JWT
    Limit czasu autoryzacji tokenu

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,78 +1,140 @@
-var express = require('express');
-var app = express();
-var sql = require('mssql');
+const express = require('express');
+const app = express();
+const sql = require('mssql');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var accessTokenSecret = ""
 
-// Connection string parameters.
+function GenerateSecret() {
+    var NewSecret = ((Math.random() + 16).toString(36).substring(7)+(Math.random() + 16).toString(36).substring(7)+(Math.random() + 16).toString(36).substring(7))
+    accessTokenSecret = NewSecret
+    module.exports = accessTokenSecret;
+}
+setInterval(GenerateSecret, 120000);
+GenerateSecret()
+console.log(accessTokenSecret)
+
 var sqlConfig = require('./sqlconfig.js');
 var server = app.listen(8081, function () {
-    var host = server.address().address
     var port = server.address().port
     console.log("Aplikacja nasłuchuje na: http://localhost:%s", port)
 });
 
-app.get('/GetMiejsca', function (req, res) {
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.end('Forbidden');
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.end('Unauthorized');
+    }
+};
+
+
+app.post('/Rejestracja', function (req, res) {
+    const { username, nazwisko, password, email} = req.body
+    let buff = new Buffer.from(password, 'base64');
+    let passwordDecoded = buff.toString('ascii');
+
+    sql.connect(sqlConfig, function() {
+        var request = new sql.Request();
+        request.input('UserName', username)
+        request.input('UserNazwisko', nazwisko)
+        request.input('Password', passwordDecoded)
+        request.input('Email', email)
+        request.execute('dbs_Parkingowy_Klient_Create', function(err) {
+            if (err) console.log(err);
+
+            res.send(JSON.stringify({Status: 200}));
+        });
+    });
+})
+
+app.post('/Login', function (req,res ) {
+    const { email, password } = req.body;
+    let buff = new Buffer.from(password, 'base64');
+    let passwordDecoded = buff.toString('ascii');
+
+    sql.connect(sqlConfig, function() {
+        var request = new sql.Request();
+        request.input('Email', email)
+        request.input('Password', passwordDecoded)
+        request.execute('dbs_Parkingowy_Klient_Login', function(err, recordset) {
+            if (err) console.log(err);
+
+            var StatusLogowania = (JSON.stringify(recordset.recordset[0].StatusLogowania))
+            var UserId = (recordset.recordset[0].UserId)
+            if (StatusLogowania = "200") {
+            const accessToken = jwt.sign({ UserId }, accessTokenSecret);
+
+            res.send(JSON.stringify({Token: accessToken}));
+        }
+            else {
+                res.send(JSON.stringify({Status: "400"}));
+            }
+
+        });
+    });
+})
+
+app.get('/GetMiejsca', authenticateJWT, function (req, res) {
     sql.connect(sqlConfig, function() {
         var request = new sql.Request();
         request.execute('dbs_Parkingowy_MiejscaWolne', function(err, recordset) {
             if (err) console.log(err);
 
-            res.end(JSON.stringify(recordset.recordset));
+            res.send(JSON.stringify(recordset.recordset));
         });
     });
 })
 
-app.get('/Rejestracja/:username/:nazwisko/:password/:email', function (req, res) {
-    let buff = new Buffer.from(req.params.password, 'base64');
-    let password = buff.toString('ascii');
-
+app.post('/ZapiszKoordynaty', function (req, res) {
+    const {KordX, KordY, UserId} = req.body
     sql.connect(sqlConfig, function() {
         var request = new sql.Request();
-        request.input('UserName', req.params.username)
-        request.input('UserNazwisko', req.params.nazwisko)
-        request.input('Password', password)
-        request.input('Email', req.params.email)
-        request.execute('dbs_Parkingowy_CreateUser', function(err) {
-            if (err) console.log(err);
-
-            res.end(JSON.stringify({Status: 200}));
-        });
-    });
-})
-
-app.get('/ZapiszKoordynaty/:KordX/:KordY/:UserID', function (req, res) {
-    sql.connect(sqlConfig, function() {
-        var request = new sql.Request();
-        request.input('UserKoordynatyX', req.params.KordX)
-        request.input('UserKoordynatyY', req.params.KordY)
-        request.input('UserId', req.params.UserID)
+        request.input('UserKoordynatyX', KordX)
+        request.input('UserKoordynatyY', KordY)
+        request.input('UserId', UserID)
         request.execute('dbs_Parkingowy_Klient_ZapiszKoordAuta', function(err) {
             if (err) console.log(err);
 
-            res.end(JSON.stringify({Status: 200}));
+            res.send(JSON.stringify({Status: 200}));
         })
     })
 })
 
-app.get('/Wjazd/:UserId', function (req, res) {
+app.post('/Wjazd', function (req, res) {
+    const UserId = req.body
     sql.connect(sqlConfig, function() {
         var request = new sql.Request();
-        request.input('UserId', req.params.UserId)
+        request.input('UserId', UserId)
         request.execute('dbs_Parkingowy_Klient_Wjazd', function(err) {
             if (err) console.log(err);
 
-            res.end(JSON.stringify({Status: 200}));
+            res.send(JSON.stringify({Status: 200}));
         })
     })
 })
 
-app.get('/Odjazd/:UserId', function (req, res) {
+app.post('/Odjazd', function (req, res) {
+    const UserId = req.body
     sql.connect(sqlConfig, function() {
         var request = new sql.Request();
-        request.input('UserId', req.params.UserId)
+        request.input('UserId', UserId)
         request.execute('dbs_Parkingowy_Klient_Odjazd', function(err) {
             if (err) console.log(err);
 
-            res.end(JSON.stringify({Status: 200}));
+            res.send(JSON.stringify({Status: 200}));
         })
     })
 })
